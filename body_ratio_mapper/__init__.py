c23ae06fd215eb5f9cb7d1eabd8e4524bf02ae60
wuwukasi <152004308+wuwukaka@users.noreply.github.com>
2026-05-05 10:58:41 +0800

    Update __init__.py

--- body_ratio_mapper/__init__.py
+++ body_ratio_mapper/__init__.py
@@ -3,21 +3,21 @@ BodyRatioMapper nodes package
 Provides organized node classes for pose rendering and transformation
 """
 
-# Import ultimate detector node class
+# Import proportion transfer node class
 from .proportion_transfer_node import (
     BodyRatioMapperProportionTransfer,
 )
 
-# Import render node classes
+# Import render node class
 from .render_nodes import (
     BodyRatioMapperSDPoseRender,
 )
 
 __all__ = [
-    # Ultimate detector node
+    # Proportion transfer node
     "BodyRatioMapperProportionTransfer",
 
-    # Render nodes
+    # Render node
     "BodyRatioMapperSDPoseRender",
 
 ]
